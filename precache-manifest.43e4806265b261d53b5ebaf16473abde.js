19fa26a748c798f5e7329596752358d6f777e39a
Toyosi Azuh <toyosi.azuh@tizeti.com>
2019-10-29 12:16:11 +0100

    Updates

---
+++ precache-manifest.43e4806265b261d53b5ebaf16473abde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4ca6c78e65bee48168a408ea0e1b39",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "8ccf132f3f7bef2f5328",
+    "url": "/Todo-List/static/css/main.421176bb.chunk.css"
+  },
+  {
+    "revision": "57da7960b4250995da41",
+    "url": "/Todo-List/static/js/2.6dc69226.chunk.js"
+  },
+  {
+    "revision": "8ccf132f3f7bef2f5328",
+    "url": "/Todo-List/static/js/main.e0ac260b.chunk.js"
+  },
+  {
+    "revision": "52a92b9bf4d764eb98c8",
+    "url": "/Todo-List/static/js/runtime-main.24fb6fc5.js"
+  }
+]);
